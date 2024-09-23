fe63f0ed422aec91f9e2b57bc73d3038617b4438
Kristiyan <146897699+Krischo99@users.noreply.github.com>
2024-09-23 14:48:06 +0400

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.example.application {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+                        requires org.kordamp.bootstrapfx.core;
+            
+    opens com.example.application to javafx.fxml;
+    exports com.example.application;
+}
