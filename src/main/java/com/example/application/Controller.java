eaac968f2c495cb243decc52d5771a74ea880a04
Kristiyan <146897699+Krischo99@users.noreply.github.com>
2024-09-26 03:12:22 +0400

    sidebar
    finish sidebar and make one test table view for debug

--- src/main/java/com/example/application/Controller.java
+++ src/main/java/com/example/application/Controller.java
@@ -1,5 +1,96 @@
 package com.example.application;
 
-public class Controller {
-    
+import javafx.animation.TranslateTransition;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.util.Duration;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    @FXML
+    private Button btnAddProduct;
+
+    @FXML
+    private Button btnBiroterapiya;
+
+    @FXML
+    private Button btnConsumer;
+
+    @FXML
+    private Button btnDeleteProduct;
+
+    @FXML
+    private ImageView btnExit;
+
+    @FXML
+    private Button btnHome;
+
+    @FXML
+    private Button btnVedena;
+
+    @FXML
+    private Label cardMenuClose;
+
+    @FXML
+    private Label cardMenuOpen;
+
+    @FXML
+    private Label exitBtn;
+
+    @FXML
+    private Label productCounter;
+
+    @FXML
+    private AnchorPane shoppingCartMenu;
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        btnExit.setOnMouseClicked(mouseEvent -> {
+            System.exit(0);
+        });
+        exitBtn.setOnMouseClicked(event -> {
+            System.exit(0);
+        });
+
+        shoppingCartMenu.setTranslateX(250);
+
+        cardMenuOpen.setOnMouseClicked(click -> {
+            TranslateTransition slide = new TranslateTransition();
+            slide.setDuration(Duration.seconds(0.4));
+            slide.setNode(shoppingCartMenu);
+
+            slide.setToX(0);
+            slide.play();
+
+            shoppingCartMenu.setTranslateX(250);
+
+            slide.setOnFinished((ActionEvent e) -> {
+                cardMenuOpen.setVisible(false);
+                cardMenuClose.setVisible(true);
+            });
+        });
+
+        cardMenuClose.setOnMouseClicked(click -> {
+            TranslateTransition slide = new TranslateTransition();
+            slide.setDuration(Duration.seconds(0.4));
+            slide.setNode(shoppingCartMenu);
+
+            slide.setToX(250);
+            slide.play();
+
+            shoppingCartMenu.setTranslateX(0);
+
+            slide.setOnFinished((ActionEvent e) -> {
+                cardMenuClose.setVisible(false);
+                cardMenuOpen.setVisible(true);
+            });
+        });
+    }
 }
--- src/main/java/com/example/application/Main.java
+++ src/main/java/com/example/application/Main.java
@@ -3,16 +3,19 @@ package com.example.application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
-public class Application extends javafx.application.Application {
+public class Main extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 900, 580);
         stage.setTitle("Request App");
         stage.setScene(scene);
+        stage.setResizable(false);
+        stage.initStyle(StageStyle.UNDECORATED);
         stage.show();
     }
 
