3cc1d0755656b7524bc9dc7a60bdfd90710594fc
Kristiyan <146897699+Krischo99@users.noreply.github.com>
2024-09-27 04:40:22 +0400

    tableviews
    add table views and add functionality to some buttons

--- src/main/java/com/example/application/Controller.java
+++ src/main/java/com/example/application/Controller.java
@@ -6,14 +6,20 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 import javafx.util.Duration;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
+
+    @FXML
+    private TableView<?> biroterapiyaTable;
 
     @FXML
     private Button btnAddProduct;
@@ -34,6 +40,12 @@ public class Controller implements Initializable {
     private Button btnHome;
 
     @FXML
+    private ImageView btnMin;
+
+    @FXML
+    private Button btnSend;
+
+    @FXML
     private Button btnVedena;
 
     @FXML
@@ -43,53 +55,66 @@ public class Controller implements Initializable {
     private Label cardMenuOpen;
 
     @FXML
+    private TableView<?> consumerTable;
+
+    @FXML
     private Label exitBtn;
+
+    @FXML
+    private BorderPane mainPanel;
 
     @FXML
     private Label productCounter;
 
     @FXML
     private AnchorPane shoppingCartMenu;
+
+    @FXML
+    private TableView<?> vedenaTable;
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        closeAction();
+        minimizeAction();
+        setSlideActionOnShoppingCartMenu();
+    }
+
+    public void closeAction() {
         btnExit.setOnMouseClicked(mouseEvent -> {
             System.exit(0);
         });
         exitBtn.setOnMouseClicked(event -> {
             System.exit(0);
         });
+    }
 
+    public void minimizeAction() {
+        btnMin.setOnMouseClicked(mouseEvent -> {
+            Stage stage = (Stage) mainPanel.getScene().getWindow();
+            stage.setIconified(true);
+        });
+    }
+
+    public void setSlideActionOnShoppingCartMenu() {
         shoppingCartMenu.setTranslateX(250);
 
-        cardMenuOpen.setOnMouseClicked(click -> {
+        setSlideAction(cardMenuOpen, 0, 250, cardMenuClose);
+        setSlideAction(cardMenuClose, 250, 0, cardMenuOpen);
+    }
+
+    public void setSlideAction(Label currentMenu, int transSlideX, int transX, Label otherMenu) {
+        currentMenu.setOnMouseClicked(click -> {
             TranslateTransition slide = new TranslateTransition();
             slide.setDuration(Duration.seconds(0.4));
             slide.setNode(shoppingCartMenu);
 
-            slide.setToX(0);
+            slide.setToX(transSlideX);
             slide.play();
 
-            shoppingCartMenu.setTranslateX(250);
+            shoppingCartMenu.setTranslateX(transX);
 
             slide.setOnFinished((ActionEvent e) -> {
-                cardMenuOpen.setVisible(false);
-                cardMenuClose.setVisible(true);
-            });
-        });
-
-        cardMenuClose.setOnMouseClicked(click -> {
-            TranslateTransition slide = new TranslateTransition();
-            slide.setDuration(Duration.seconds(0.4));
-            slide.setNode(shoppingCartMenu);
-
-            slide.setToX(250);
-            slide.play();
-
-            shoppingCartMenu.setTranslateX(0);
-
-            slide.setOnFinished((ActionEvent e) -> {
-                cardMenuClose.setVisible(false);
-                cardMenuOpen.setVisible(true);
+                currentMenu.setVisible(false);
+                otherMenu.setVisible(true);
             });
         });
     }
